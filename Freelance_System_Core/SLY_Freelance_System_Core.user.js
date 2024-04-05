4af9c6bf9a8727fda96f9162ba93457f8b5a4633
zio731 <137715274+zio731@users.noreply.github.com>
2024-04-05 22:48:35 +0200

    Update SLY_Freelance_System_Core.user.js

--- Freelance_System_Core/SLY_Freelance_System_Core.user.js
+++ Freelance_System_Core/SLY_Freelance_System_Core.user.js
@@ -6,9 +6,9 @@
 // @author       SLY
 // @match        https://based.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/zio731/starbased/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/zio731/starbased/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/zio731/starbased/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
